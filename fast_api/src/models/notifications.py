c909b6922c16c0d6b91f33900e63f6ab8e11f61c
NatalyaLaktyushkina <tigerclow@mail.ru>
2022-10-08 23:55:37 +0300

    scheduler: initial

--- fast_api/src/models/notifications.py
+++ fast_api/src/models/notifications.py
@@ -1,5 +1,11 @@
 """Description of notification models."""
 
+from datetime import datetime
+from enum import Enum
+from typing import Dict
+
+from pydantic import BaseModel
+
 from models.json_config import BaseOrjsonModel
 
 
@@ -7,3 +13,21 @@ class NotificationSent(BaseOrjsonModel):
     """This is the description of critique post response  model."""
 
     notification_sent: bool
+
+
+class Source(str, Enum):
+    email = 'email'
+    sms = 'sms'
+    push = 'push'
+
+
+class EventType(str, Enum):
+    welcome_letter = 'welcome_letter'
+    critique_likes = 'critique_likes'
+
+
+class Event(BaseModel):
+    source: Source
+    event_type: EventType
+    scheduled_datetime: datetime
+    payload: Dict
