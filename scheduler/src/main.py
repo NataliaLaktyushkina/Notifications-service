65bcc89424a9515b79ace234845b6cad09f133a6
NatalyaLaktyushkina <tigerclow@mail.ru>
2022-10-09 22:55:32 +0300

    simple events generator

--- scheduler/src/main.py
+++ scheduler/src/main.py
@@ -1,5 +1,46 @@
-def main() -> None:
-    pass
+from models.events import Event, Source, EventType
+from datetime import datetime
+from core.config import settings
+import uuid
+from random import randint
+
+NUMBER_EVENTS = settings.NUMBER_EVENTS
+
+
+def get_payload_likes() -> dict:
+    user_id = uuid.uuid4()
+    movie_id = uuid.uuid4()
+    n_likes = randint(1, 10)  # noqa: S311
+    payload = {'user':
+                   {'user_id': user_id },
+               'content': [
+                   {'movie_id': movie_id,
+                    'likes': n_likes,
+                    },
+               ],
+               }
+    return payload
+
+
+def generate_event() -> Event:
+    source = Source.email
+    event_type = EventType.likes_number
+    scheduled_time = datetime(2022, 10, 9, 22, 30)
+    payload = get_payload_likes()
+    return Event(source=source,
+                 event_type=event_type,
+                 scheduled_datetime=scheduled_time,
+                 payload=payload,
+                 )
+
+
+def main() -> list:
+    events_list = []
+    for _i in range(NUMBER_EVENTS):
+        event = generate_event()
+        events_list.append(event)
+    print(events_list)
+    return events_list
 
 
 if __name__ == '__main__':
---
+++ scheduler/src/models/events.py
@@ -0,0 +1,25 @@
+"""Description of notification models."""
+
+from datetime import datetime
+from enum import Enum
+from typing import Dict
+
+from pydantic import BaseModel
+
+
+class Source(str, Enum):
+    email = 'email'
+    sms = 'sms'
+    push = 'push'
+
+
+class EventType(str, Enum):
+    welcome_letter = 'welcome_letter'
+    likes_number = 'likes_number'
+
+
+class Event(BaseModel):
+    source: Source
+    event_type: EventType
+    scheduled_datetime: datetime
+    payload: Dict
